61641155971117674dec87f7f3b0bb643a53ceff
Piotr Cieslak <eu.piotr@gmail.com>
2021-10-08 09:13:21 +0200

    gitignore .env added

--- yt-mosh/firstProject/firstProject/settings.py
+++ yt-mosh/firstProject/firstProject/settings.py
@@ -8,6 +8,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 from pathlib import Path
 
+" secret hiding"
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -15,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-22_==i@^!_a9wm%ja3q_dr(_tku1y#r2v*y^j@eu8887^pu8_c'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
